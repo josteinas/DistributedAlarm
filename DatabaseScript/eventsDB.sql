d4e3ce2a6ea7c5dd5e544308f07e3940c8281844
Josteinas <josteinsolaas@gmail.com>
2014-06-25 22:16:55 +0200

    friends and private boolean in dbScript

--- DatabaseScript/eventsDB.sql
+++ DatabaseScript/eventsDB.sql
@@ -7,11 +7,20 @@ create table if not exists User (
 	primary key (username)
 ) engine = InnoDB default charset = utf8;
 
+create table if not exists Friends (
+	user1 varchar(30),
+	user2 varchar(30),
+	primary key (user1,user2),
+	constraint user1_fk foreign key (user1) references User(username) on delete cascade on update cascade,
+	constraint user2_fk foreign key (user2) references User(username) on delete cascade on update cascade
+) engine=InnoDB default charset=utf8;
+
 create table if not exists Category (
 	category_id mediumint not null auto_increment,
 	name varchar(30) not null,
 	lat float not null, 
 	lon float not null,
+	private boolean,
 	creator varchar(30),
 	primary key (category_id),
 	constraint category_creator foreign key (creator) references User(Username) on delete cascade on update cascade
@@ -22,6 +31,7 @@ create table if not exists Happening (
 	starttime datetime,
 	endtime datetime,
 	category_id mediumint,
+	private boolean,
 	constraint happening_category foreign key (category_id) references Category(category_id) on delete cascade on update cascade,
 	primary key (happening_id)
 ) engine= InnoDB default charset = utf8;
